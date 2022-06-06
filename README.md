8b18b4e5f83f2345628ad204f3842313edf31823
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-06-06 16:27:31 +0900

    impl. simple converters

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 [![PkgGoDev](https://pkg.go.dev/badge/github.com/koron/cvs2avro)](https://pkg.go.dev/github.com/koron/cvs2avro)
 [![Actions/Go](https://github.com/koron/cvs2avro/workflows/Go/badge.svg)](https://github.com/koron/cvs2avro/actions?query=workflow%3AGo)
 [![Go Report Card](https://goreportcard.com/badge/github.com/koron/cvs2avro)](https://goreportcard.com/report/github.com/koron/cvs2avro)
+
+## Supported Avro's types
+
+* `string`
+* `int`
+* `long`
+* `float`
+* `double`
+* `boolean`
+* `null` - always null.
--- cvs2avro.go
+++ cvs2avro.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/hamba/avro"
 )
@@ -13,14 +14,87 @@ type Converter interface {
 	Convert(string) (interface{}, error)
 }
 
+type convertFunc func(string) (interface{}, error)
+
+func (f convertFunc) Convert(s string) (interface{}, error) {
+	return f(s)
+}
+
 type fieldConverter struct {
 	name string
 	conv Converter
 }
 
-func newFieldConverter(*avro.Field) (fieldConverter, error) {
-	// TODO:
-	return fieldConverter{}, nil
+func newFieldConverter(f *avro.Field) (fieldConverter, error) {
+	switch f.Type().Type() {
+	case avro.String:
+		return fieldConverter{
+			name: "string",
+			conv: convertFunc(func(s string) (interface{}, error) {
+				return s, nil
+			}),
+		}, nil
+
+	case avro.Int:
+		return fieldConverter{
+			name: "int",
+			conv: convertFunc(func(s string) (interface{}, error) {
+				return strconv.ParseInt(s, 10, 32)
+			}),
+		}, nil
+
+	case avro.Long:
+		return fieldConverter{
+			name: "long",
+			conv: convertFunc(func(s string) (interface{}, error) {
+				return strconv.ParseInt(s, 10, 64)
+			}),
+		}, nil
+
+	case avro.Float:
+		return fieldConverter{
+			name: "float",
+			conv: convertFunc(func(s string) (interface{}, error) {
+				return strconv.ParseFloat(s, 32)
+			}),
+		}, nil
+
+	case avro.Double:
+		return fieldConverter{
+			name: "double",
+			conv: convertFunc(func(s string) (interface{}, error) {
+				return strconv.ParseFloat(s, 64)
+			}),
+		}, nil
+
+	case avro.Boolean:
+		return fieldConverter{
+			name: "bool",
+			conv: convertFunc(func(s string) (interface{}, error) {
+				return strconv.ParseBool(s)
+			}),
+		}, nil
+
+	case avro.Null:
+		return fieldConverter{
+			name: "null",
+			conv: convertFunc(func(s string) (interface{}, error) {
+				return nil, nil
+			}),
+		}, nil
+
+	default:
+		return fieldConverter{}, fmt.Errorf("unsupported type: %s", f.Type().Type())
+	//case avro.Record:
+	//case avro.Error:
+	//case avro.Ref:
+	//case avro.Enum:
+	//case avro.Array:
+	//case avro.Map:
+	//case avro.Union:
+	//case avro.Fixed:
+	//case avro.Bytes:
+	}
 }
 
 type recordConverter struct {
